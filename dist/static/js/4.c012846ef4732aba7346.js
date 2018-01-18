b4e54dbbbba18cdbce471965c1f0659026db9c34
hjk <849989428@qq.com>
2018-01-18 10:31:24 +0800

    Add Build files

---
+++ dist/static/js/4.c012846ef4732aba7346.js
@@ -0,0 +1,2 @@
+webpackJsonp([4],{"3Tqg":function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var r={render:function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"welcome"},[n("h2",[e._v("当前登录用户："+e._s(e.$store.state.userinfo.username))])])},staticRenderFns:[]},s=n("VU/8")({name:"welcome",data:function(){return{}}},r,!1,null,null,null);t.default=s.exports}});
+//# sourceMappingURL=4.c012846ef4732aba7346.js.map
